9f924d2aa9b720de980bbcb0699862b9fc6c2c7d
Jan Majerníček <arcanusvontenebris@gmail.com>
2022-07-11 08:30:42 +0200

    Create left article section with styling

--- src/components/Article/Left.tsx
+++ src/components/Article/Left.tsx
@@ -1,8 +1,27 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowCircleLeft } from "@fortawesome/free-solid-svg-icons";
+
+import styles from "./Left.module.css";
+import { Link } from "react-router-dom";
+
 const Left = (props: { title: string; description: string; color: string }) => {
-  return <div style={{"backgroundColor": props.color}}>
-    <h2>{props.title}</h2>
-    <p>{props.description}</p>
-  </div>;
+  return (
+    <div className={styles.left} style={{ backgroundColor: props.color }}>
+      <div>
+        <h2>{props.title}</h2>
+        <p>{props.description}</p>
+      </div>
+      <Link to="/">
+        <div className={styles.back}>
+          <FontAwesomeIcon
+            icon={faArrowCircleLeft}
+            className={styles["back__icon"]}
+          />{" "}
+          <p>Back on track</p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Left;
